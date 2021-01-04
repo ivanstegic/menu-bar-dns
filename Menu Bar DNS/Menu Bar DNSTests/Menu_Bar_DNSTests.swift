a1e1a8206d803b4e0c38f4564c51e3b88cfcf8ff
Munir Ahmed <mnirahd@gmail.com>
2021-01-04 15:20:51 +0500

    Issue #1: Create Xcode project that compiles

---
+++ Menu Bar DNS/Menu Bar DNSTests/Menu_Bar_DNSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Menu_Bar_DNSTests.swift
+//  Menu Bar DNSTests
+//
+//  Created by Munir Ahmed on 04/01/2021.
+//
+
+import XCTest
+@testable import Menu_Bar_DNS
+
+class Menu_Bar_DNSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
